cb138f09f074dfd69bc9e00881ab795b8603b63e
Ishita Panchal <ishitapanchal146@gmail.com>
2026-05-06 13:10:51 +0530

    Add files via upload

--- InputExample/src/Addapp.java
+++ InputExample/src/Addapp.java
@@ -4,10 +4,10 @@ public class Addapp {
 public static void main(String args[])
 {
 java.util.Scanner sc = new java.util.Scanner(System.in);
-System.out.println("Enter a no");
+System.out.println("Enter a no:");
 int a=sc.nextInt(),b=sc.nextInt();
 int sum=a+b;
-System.out.println("SUm is "+sum);
+System.out.println("Sum is: "+sum);
 
 }
 }
